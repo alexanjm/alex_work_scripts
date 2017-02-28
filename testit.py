963f515ff5632f2247c42e294a0307fc20226a20
Alex Jones <alexjones1725@gmail.com>
2017-02-28 14:45:43 -0500

    Complete refactor -- removed instances of exec() for cleaner functions

--- testit.py
+++ testit.py
@@ -2,7 +2,18 @@
 # -*- coding: utf-8 -*-
 # Created on: Feb 27, 2017
 
-
+col_list = []
 for i in range(10):
-    exec("a = {0}".format(i))
-        print(a)
+    #list_str = "a.%d = ['alex']" % i
+    #exec("a.%d = ['alex']" % i)
+
+    #print(a)
+
+    #exec("list_str%s = []" % (i))
+    a = "col{0}".format(i + 1)
+    print(a)
+    a = []
+    col_list.append(a)
+    print(col_list)
+
+
--- ultimate_parser.py
+++ ultimate_parser.py
@@ -5,45 +5,16 @@
 # import os
 import sys
 import argparse
-
-# test_c = 3
-# for x in range(test_c):
-#     list_name = str(x)
-#     list_name = []
-#     print(list_name)
-# one.append('alex')
-# print(one)
-
-# for x in cols:
-#     print(x)
-#     x = []
-#     print(x)
-#     x.append('yay')
-
-
-if __name__ == '__main__':
-    args = sys.argv
-    parser = argparse.ArgumentParser(description='GOAL: Parse anything and everything however you would like')
-    parser.add_argument('input_file', help='transdecoder file')
-    parser.add_argument('-id', '--in_delim', help='input delimiter')
-    parser.add_argument('-od', '--out_delim', help='output delimiter')
-    parser.add_argument('-s', '--sort', help='sort column <#> by <method> (ascending, descending, alphabetical')
-    parser.add_argument('-o', '--out_file', help='output file')
-    parser.add_argument('-c', '--column', help='columns #s (1-?) to write to output')
-    parser.add_argument('-hd', '--header', default='y', help='header? <y or n>')
-    in_args = parser.parse_args()
-
+def get_delimiter():
     with open(in_args.input_file, 'r') as ifile:
         first_line = ifile.readline()
         print('arg', in_args.in_delim)
         if in_args.in_delim is None:
-            print('None')
             if '\t' in first_line:
                 delim_str = '\t'
             else:
                 delim_str = ','
         else:
-            print('some')
             if in_args.in_delim == 't':
                 delim_str = '\t'
             else:
@@ -51,43 +22,55 @@ if __name__ == '__main__':
         print('delimiter', delim_str)
         num_dels = first_line.count(delim_str)
     ifile.close()
-    # print('delimiter is', delim_str)
+    return delim_str, num_dels
 
-    if in_args.header == 'y':
-        first_line = first_line.split(delim_str)
-        # print(first_line)
-        for i in range(num_dels + 1):
-            exec("col%d = []" % (i + 1))
-
-        with open(in_args.input_file, 'r') as ifile:
-            for line in ifile:
-                line = line.rstrip('\n').split(delim_str)
-                # print('line', line)
-                for i in range(num_dels + 1):
-                    exec("col%d.append(line[%d])" % (i + 1, i))
-                    # print('i is:', i)
-
+def out_file_name():
     if in_args.out_file is True:
         ofile_name = in_args.out_file
     else:
         ofile_name = in_args.input_file + '.parsed'
     print(ofile_name)
+    return ofile_name
+
+def get_num_cols():
+    col_name_list = []
+    for i in range(num_dels + 1):
+        list_str = "col{0}".format(i + 1)
+        print('list', list_str)
+        list_str = []
+        col_name_list.append(list_str)
+    return col_name_list
+
+def read_infile():
+    with open(in_args.input_file, 'r') as ifile:
+        for line in ifile:
+            line = line.rstrip('\n').split(delim_str)
+            for i in range(num_dels + 1):
+                cols[i].append(line[i])
+    return cols
+
+def write_outfile():
     out_str = ''
     with open(ofile_name, 'w') as ofile:
-        for x in range(len(col1)):
+        for x in range(len(cols[0])):
             # print(col1[x])
             # out_str = ''
-            for y in range(1, num_dels+2):
-                exec("a = col{0}".format(y))
-                if delim_str == '\t':
-                    out_delim = ','
+            for y in range(num_dels + 1):
+                if in_args.in_delim is None:
+                    if delim_str == '\t':
+                        out_delim = ','
+                    else:
+                        out_delim = '\t'
                 else:
-                    out_delim = '\t'
+                    if in_args.in_delim == 't':
+                        out_delim = ','
+                    else:
+                        out_delim = '\t'
                 # print('out delimiter', out_delim)
-                out_str_col = '%s' % (a[x])
+                out_str_col = '%s' % (cols[y][x])
                 out_str_delim = out_str_col + out_delim
                 # print(out_str_delim)
-                if y < (num_dels + 1):
+                if y < (num_dels):
                     out_str = out_str + out_str_delim
                     # print('one', out_str)
                 else:
@@ -96,3 +79,30 @@ if __name__ == '__main__':
         # this is final output in tabular form -- still need to re-put headers
         print(out_str)
         ofile.write(out_str)
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    parser = argparse.ArgumentParser(description='GOAL: Parse anything and everything however you would like')
+    parser.add_argument('input_file', help='transdecoder file')
+    parser.add_argument('-d', '--in_delim', help='input delimiter')
+    parser.add_argument('-s', '--sort', help='sort column <#> by <method> (ascending, descending, alphabetical')
+    parser.add_argument('-o', '--out_file', help='output file')
+    parser.add_argument('-c', '--column', help='columns #s (1-?) to write to output')
+    parser.add_argument('-hd', '--header', default='y', help='header? <y or n>')
+    in_args = parser.parse_args()
+
+    delim_str, num_dels = get_delimiter()
+    print('dels', num_dels)
+
+    cols = get_num_cols()
+    print('cols', cols)
+
+    cols_list = read_infile()
+    print('cols list', cols_list)
+
+    ofile_name = out_file_name()
+
+    write_outfile()
+
+
