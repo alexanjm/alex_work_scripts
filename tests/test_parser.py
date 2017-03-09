a16db797748bba50cebef01a5a4a93d2f5a55f7b
Alex Jones <alexjones1725@gmail.com>
2017-03-09 16:00:26 -0500

    cleaned up some functions

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -4,13 +4,11 @@
 
 from alex_work_scripts import ultimate_parser
 import pytest
-import os
+# import os
 import sys
 
 
 def test_delimiters():
-    #out1 = ultimate_parser.main()
-    #out2 = ultimate_parser.main()
     out1 = ultimate_parser.get_delimiter('tests/test_commas.txt', None)
     out2 = ultimate_parser.get_delimiter('tests/test_tabs.txt', None)
     print(out1)
@@ -22,13 +20,13 @@ def test_delimiters():
 
 
 def test_manual_delimiter1():
-    output = ultimate_parser.get_delimiter('tests/test_commas.txt', 'c')
+    output = ultimate_parser.get_delimiter('tests/test_commas.txt', ',')
 
     assert output[0] == ','
 
 
 def test_manual_delimiter2():
-    output = ultimate_parser.get_delimiter('tests/test_tabs.txt', 't')
+    output = ultimate_parser.get_delimiter('tests/test_tabs.txt', '\t')
 
     assert output[0] == '\t'
 
@@ -57,7 +55,7 @@ def test_get_num_cols():
 
 
 def test_read_infile():
-    column_contents = ultimate_parser.read_infile('tests/test_commas.txt', ',', 2, [[], [], []])
+    column_contents = ultimate_parser.read_infile('tests/test_commas.txt', ',', 2, [[], [], []], 0)
     result = [['alex', '1', '4'], ['ryan', '2', '5'], ['kimani', '3', '6']]
 
     assert column_contents == result
@@ -66,7 +64,7 @@ def test_read_infile():
 def test_write_outfile():
     ofile = 'tests/test_commas.txt.parsed'
     cols_list = [['alex', '1', '4'], ['ryan', '2', '5'], ['kimani', '3', '6']]
-    output = ultimate_parser.write_outfile(None, ofile, cols_list, 2, True, ',', None)
+    output = ultimate_parser.write_outfile(None, ofile, cols_list, 2, True, ',', None, None)
     result = ['alex\tryan\tkimani', '1\t2\t3', '4\t5\t6']
     lines = []
     with open(output, 'r') as ofile:
@@ -80,7 +78,7 @@ def test_write_outfile():
 def test_write_outfile_input_delims_columns_parse():
     ofile = 'tests/test_commas.txt.parsed'
     cols_list = [['alex', '1', '4'], ['ryan', '2', '5'], ['kimani', '3', '6']]
-    output = ultimate_parser.write_outfile('3,1', ofile, cols_list, 2, True, ',', 'c')
+    output = ultimate_parser.write_outfile('3,1', ofile, cols_list, 2, True, ',', None, None)
     result = ['kimani\talex', '3\t1', '6\t4']
     lines = []
     with open(output, 'r') as ofile:
@@ -93,31 +91,43 @@ def test_write_outfile_input_delims_columns_parse():
 
 def test_sort_ascending_no_header():
     transposed_list = [['alex', '1', '1'], ['zeke', '291', '-9'], ['peter', '12', '92'], ['kimani', '2', '-4']]
-    sort_input = '2,a'
+    sort_input = [3, False]
 
-    sorted = [['zeke', 291.0, -9.0], ['kimani', 2.0, -4.0], ['alex', 1.0, 1.0], ['peter', 12.0, 92.0]]
+    sorted_list = [['zeke', 291.0, -9.0], ['kimani', 2.0, -4.0], ['alex', 1.0, 1.0], ['peter', 12.0, 92.0]]
     output = ultimate_parser.sort_file(transposed_list, sort_input)
 
-    assert sorted == output
+    assert sorted_list == output
+
+
+def test_diff_col_nums():
+    delimiter_string, number_delimiters, diff_columns = ultimate_parser.get_delimiter('tests/bad_header.txt', None)
+
+    assert diff_columns == -1
+
+    column_contents_list = ultimate_parser.read_infile('tests/bad_header.txt', '\t', 2, [[], [], []], diff_columns)
+
+    result = [['Column Name', 'alex', 'ryan', 'kimani'], ['age', '24', '22', '23'], ['weight', '195', '190', '160']]
+
+    assert column_contents_list == result
 
 
 def test_main(monkeypatch):
-    monkeypatch.setattr(sys, "argv", ['ultimate_parser', 'tests/sorted_test_header.txt', '-p', '-hd', '-sd', '2'])
+    monkeypatch.setattr(sys, "argv", ['ultimate_parser', 'tests/sorted_test_header.txt', '-p', '-hd', '-sd', '3'])
     ofile = ultimate_parser.main()
     o_name = 'tests/sorted_test_header.txt' + '.parsed' + '.sorted'
 
     assert ofile == o_name
 
-    result = ['one,two,three','peter,12.0,92.0','alex,1.0,1.0','kimani,2.0,-4.0','zeke,291.0,-9.0']
+    result = ['one,two,three', 'peter,12.0,92.0', 'alex,1.0,1.0', 'kimani,2.0,-4.0', 'zeke,291.0,-9.0']
     lines = []
     results = []
     with open(o_name, 'r') as ofile:
         for line in ofile:
             line = line.strip('\n')
             lines.append(line)
-        for x in result:
-            results.append(x)
+    for x in result:
+        results.append(x)
 
-        assert lines == results
+    assert lines == results
 
 
--- ultimate_parser.py
+++ ultimate_parser.py
@@ -11,36 +11,34 @@ def get_delimiter(in_file, in_delim):
     with open(in_file, 'r') as ifile:
         first_line = ifile.readline()
         second_line = ifile.readline()
-        # first_length = len(first_line.split('\t'))
-        # second_length = len(second_line.split('\t'))
-        # print('arg', in_args.in_delim)
         if in_delim is None:
             if '\t' in first_line:
                 delim_str = '\t'
             else:
                 delim_str = ','
         else:
-            if in_delim == 't':
-                delim_str = '\t'
-            else:
-                delim_str = ','
-        # print('delimiter', delim_str)
+            delim_str = in_delim
+            print(delim_str)
+
         num_dels = second_line.count(delim_str)
+        first_length = len(first_line.split(delim_str))
+        second_length = len(second_line.split(delim_str))
+        diff = first_length - second_length
     ifile.close()
-    return delim_str, num_dels
+    return delim_str, num_dels, diff
 
 
-def get_out_file_name(in_file, out_file, parsed, sorted):
+def get_out_file_name(in_file, out_file, parsed, is_sorted):
     out_file_name = in_file
     if out_file is not None:
         out_file_name = out_file
     else:
         if parsed is True:
             out_file_name = out_file_name + '.parsed'
-        if sorted is not None:
+        if is_sorted is not None:
             out_file_name = out_file_name + '.sorted'
-        if parsed is False and sorted is None:
-            out_file_name = out_file_name + '.out'
+        if parsed is False and is_sorted is None:
+            out_file_name = out_file_name + '.parsed'
 
     return out_file_name
 
@@ -49,15 +47,13 @@ def get_num_cols(col_nums):
     col_name_list = []
     for i in range(col_nums + 1):
         list_str = "col{0}".format(i + 1)
-        # print('list', list_str)
         list_str = []
         col_name_list.append(list_str)
     return col_name_list
 
 
-def read_infile(input_file, delim, col_nums, columns_list):
+def read_infile(input_file, delim, col_nums, columns_list, diff):
     with open(input_file, 'r') as ifile:
-        #print(col_nums)
         for line in ifile:
             line = line.rstrip('\n').split(delim)
             for i in range(col_nums + 1):
@@ -67,19 +63,32 @@ def read_infile(input_file, delim, col_nums, columns_list):
                     columns_list[i].append('Column Name')
     # It will do the following correction every time - however if 'Column Name' already exists at the front of the
     # file it will remain there -- can potentially put this into an if statement or if an error is raised
-    col_titles = []
-    for x in columns_list:
-        col_titles.append(x[0])
-    idx = col_titles.index('Column Name')
-    new_col_titles = col_titles[idx:] + col_titles[:idx]
-    z = 0
-    for y in columns_list:
-        y[0] = new_col_titles[z]
-        z += 1
+    if diff != 0:
+        col_titles = []
+        for x in columns_list:
+            col_titles.append(x[0])
+        idx = col_titles.index('Column Name')
+        new_col_titles = col_titles[idx:] + col_titles[:idx]
+        z = 0
+        for y in columns_list:
+            y[0] = new_col_titles[z]
+            z += 1
 
     ifile.close()
     return columns_list
 
+
+def how_to_sort(asc, desc):
+    sorting = [asc, desc]
+
+    if sorting[1] is not None:
+        sort_by = [sorting[1], True]
+    elif sorting[0] is not None:
+        sort_by = [sorting[0], False]
+    else:
+        sort_by = None
+
+    return sort_by
 
 def transpose_list(column_list):
     transposed = [[column_list[j][i] for j in range(len(column_list))] for i in range(len(column_list[0]))]
@@ -96,7 +105,7 @@ def transpose_list(column_list):
 #     else:
 #         return transposed_list, None
 
-##make different input for different types of sorting -sort_asc -sort_desc -- easier
+
 def sort_file(column_lists, sort_input):
     for i in range(len(column_lists)):
         for j in range(len(column_lists[0])):
@@ -107,35 +116,23 @@ def sort_file(column_lists, sort_input):
             except ValueError:
                 pass
 
-    in_sort = sort_input.split(',')
-    # try:
-    col_sort = int(in_sort[0]) - 1
-    # except ValueError:
-    #     print("ERROR: Please se")
-    sort_method = in_sort[1].strip()
-    if sort_method == 'r':
-        sorted_cols = sorted(column_lists, key=lambda my_sort_cols: my_sort_cols[col_sort], reverse=True)
-    else:
-        sorted_cols = sorted(column_lists, key=lambda my_sort_cols: my_sort_cols[col_sort])
+    col_sort = sort_input[0] - 1
+    sort_method = sort_input[1]
+    sorted_cols = sorted(column_lists, key=lambda my_sort_cols: my_sort_cols[col_sort], reverse=sort_method)
+
 
     return sorted_cols
 
 
-def write_outfile(columns,  ofile_name, final_cols, num_dels, in_parse, delim_str, input_delim):
+def write_outfile(columns,  ofile_name, final_cols, num_dels, in_parse, delim_str, input_delim, output_delim):
     out_str = ''
     with open(ofile_name, 'w') as ofile:
         i = 0
         for x in range(len(final_cols[0])):
-            # print(col1[x])
-            # out_str = ''
             if columns is not None:
-                # print('whatttt', columns)
                 column_nums = columns.strip().split(',')
-                # print('?', column_nums[0])
             else:
                 column_nums = range(1, num_dels + 2)
-            # print('dels', num_dels)
-            # print('cols', len(column_nums))
             for y in column_nums:
                 y = int(y) - 1
                 if in_parse is False:
@@ -147,25 +144,18 @@ def write_outfile(columns,  ofile_name, final_cols, num_dels, in_parse, delim_st
                         else:
                             out_delim = '\t'
                     else:
-                        if input_delim == 't':
-                            out_delim = ','
-                        else:
+                        if output_delim is None:
                             out_delim = '\t'
-                # print('out delimiter', out_delim)
+                        else:
+                            out_delim = output_delim
                 out_str_col = '%s' % (final_cols[y][x])
                 out_str_delim = out_str_col + out_delim
-                # print(out_str_delim)
                 if i < len(column_nums) - 1:
-                    # print('num_test', len(column_nums)-1)
-                    # print('i_test', i)
                     i += 1
                     out_str += out_str_delim
-                    # print('one', out_str)
                 else:
                     out_str += out_str_delim.rstrip(out_delim) + '\n'
                     i = 0
-                    # print('two', out_str)
-        # this is final output in tabular form -- still need to re-put headers
         ofile.write(out_str)
     ofile.close()
     return ofile_name
@@ -173,45 +163,41 @@ def write_outfile(columns,  ofile_name, final_cols, num_dels, in_parse, delim_st
 
 def main():
     #args = sys.argv
-    #print('these args', args)
     parser = argparse.ArgumentParser(description='GOAL: Parse anything and everything however you would like')
-    parser.add_argument('input_file', help='transdecoder file')
-    parser.add_argument('-d', '--in_delim', help='input delimiter')
-    parser.add_argument('-sa', '--sort_asc', help='sort column <#> by ascending order (Default sort method)')
-    parser.add_argument('-sd', '--sort_desc', help='sort column <#> by descending order')
-    # outfile functionality? not sure
-    parser.add_argument('-o', '--out_file', help='output file')
-    parser.add_argument('-c', '--column', help='columns #s (1-?) to write to output')
-    parser.add_argument('-hd', '--header',  help='-hd flag implies file has header -- default is no header',
+    parser.add_argument('input_file', help='File to be parsed.')
+    parser.add_argument('-p', '--parse', help='Convert files to comma separated, tab separated or even custom '
+                                              'delimiters. Default = False', action='store_true')
+    parser.add_argument('-hd', '--header',  help='<-hd> File contains a header?. Default = False.',
                         action='store_true')
-    parser.add_argument('-p', '--parse', help='parse -- convert delimiter', action='store_true')
+    parser.add_argument('-c', '--column', help='<int,int(...)> Column numbers to output.')
+    parser.add_argument('-sa', '--sort_asc', help='<int> Sort column in ascending order by this column number.',
+                        type=int)
+    parser.add_argument('-sd', '--sort_desc', help='<int> Sort column in descending order by this column number.',
+                        type=int)
+    # outfile functionality? not sure
+    parser.add_argument('-o', '--out_file', help='Output file location.')
+    parser.add_argument('-id', '--idel', help='<str> Custom input delimiter.', type=str)
+    parser.add_argument('-od', '--odel', help='<str> Custom output delimiter.', type=str)
     in_args = parser.parse_args()
 
-    delim_str, num_dels = get_delimiter(in_args.input_file, in_args.in_delim)
+    delim_str, num_dels, diff_cols = get_delimiter(in_args.input_file, in_args.idel)
 
     empty_cols = get_num_cols(num_dels)
 
-    cols_list = read_infile(in_args.input_file, delim_str, num_dels, empty_cols)
-    sorting = [in_args.sort_asc, in_args.sort_desc]
+    cols_list = read_infile(in_args.input_file, delim_str, num_dels, empty_cols, diff_cols)
 
-    if sorting[1] is not None:
-        sort_by = '%s,r' % sorting[1]
-    elif sorting[0] is not None:
-        sort_by = '%s,a' % sorting[0]
-    else:
-        sort_by = None
+    sorting = how_to_sort(in_args.sort_asc, in_args.sort_desc)
+
     try:
-        if sort_by is not None:
+        if sorting is not None:
             transposed = transpose_list(cols_list)
-
             if in_args.header is True:
                 header_line = transposed[0]
                 transposed.remove(header_line)
-                transposed_list_no_header = sort_file(transposed, sort_by)
+                transposed_list_no_header = sort_file(transposed, sorting)
                 transposed_list = [header_line] + transposed_list_no_header
-                # print(transposed_list)
             else:
-                transposed_list = sort_file(transposed, sort_by)
+                transposed_list = sort_file(transposed, sorting)
 
             cols_list = transpose_list(transposed_list)
 
@@ -219,9 +205,9 @@ def main():
         print("ERROR: Do you need to specify this file has a header? --> use flag -hd")
         raise SystemExit
 
-    ofile_name = get_out_file_name(in_args.input_file, in_args.out_file, in_args.parse, sort_by)
+    ofile_name = get_out_file_name(in_args.input_file, in_args.out_file, in_args.parse, sorting)
 
-    output = write_outfile(in_args.column, ofile_name, cols_list, num_dels, in_args.parse, delim_str, in_args.in_delim)
+    output = write_outfile(in_args.column, ofile_name, cols_list, num_dels, in_args.parse, delim_str, in_args.idel, in_args.odel)
 
     return output
 
