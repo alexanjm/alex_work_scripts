39660ba2732cf12e3b7c1f62508e68a11d2dd40f
Alex Jones <alexjones1725@gmail.com>
2017-06-01 11:30:33 -0400

    Working command line program to grab top hits from blast output

---
+++ grab_blast_tophit.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created on: May 31, 2017
+
+import argparse
+
+def test(input_file):
+    # input_file = '/Users/jonesalm/Documents/playground/regen_hydractinia_related/annotation/hydra_blast_sofia/transdecoder_Hech_hydraNR.results'
+    # output_file = '/Users/jonesalm/Documents/playground/regen_hydractinia_related/annotation/hydra_blast_sofia/transdecoder_Hech_hydraNR.results.top_hit'
+    output_file = input_file + '.top_hit'
+    final_data = {}
+    with open(output_file, 'w') as ofile:
+        with open(input_file, 'r') as ifile:
+            for line in ifile:
+                if line.startswith('#') == False:
+                    transcript, info = line.rstrip('\n').split('\t', 1)
+                    if bool(transcript in final_data.keys()) is False:
+                        final_data[transcript] = info
+                        ofile.write(line)
+        ifile.close()
+    ofile.close()
+
+if __name__ == '__main__':
+
+
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('input_file', help='Input file')
+    in_args = parser.parse_args()
+
+    print(in_args.input_file + '.top_hit')
+
+    test(in_args.input_file)
+
+    # to test speed -- probably need to hard code input/output files to make the below run correctly...
+    # import timeit
+    # print(timeit.repeat("test()", setup="from __main__ import test", number=1, repeat=1))
