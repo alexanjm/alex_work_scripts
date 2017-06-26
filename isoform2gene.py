5920e0887388ec0ce36d41a409d0d6399becf1d7
Alex Jones <alexjones1725@gmail.com>
2017-06-26 11:22:11 -0400

    Cleaned up isoform2gene.py

--- isoform2gene.py
+++ isoform2gene.py
@@ -11,29 +11,41 @@ import argparse
 def convert(input_file):
     output_file = input_file + '.genes'
     gene_dict = {}
+    bumped_dict = {}
     # input file is blast output with annotation appended
-    with open(input_file, 'r') as ifile:
-        for line in ifile:
-            elements = line.rstrip('\n').split('\t')
-            evalue = float(elements[9])
-            # set e value cutoff for blast hits
-            evalue_cutoff = 0.00001
-            if evalue < evalue_cutoff:
-                annotation = elements[14].strip('\x00')
-                elements[14] = annotation
-                isoform = elements[0].split('|')[0]
-                # split the isoform number off from the end of each ID
-                gene = isoform.rsplit('_', 1)[0]
-                # If the gene already exists in the gene dictionary, use the hit with the lower evalue
-                if bool(gene in gene_dict.keys()) is True:
-                    # print(gene, gene_dict[gene][9])
-                    # print(float(gene_dict[gene][9]), float(evalue))
-                    # print(float(gene_dict[gene][9]) > float(evalue))
-                    if float(gene_dict[gene][9]) > float(evalue):
-                        gene_dict[gene] = elements
-                else:
-                    gene_dict[gene] = elements
-    ifile.close()
+    bumped_file = input_file + '.insignificant_hits'
+    sig_file = input_file + '.significant_hits'
+    with open(bumped_file, 'w') as ofile2:
+        with open(sig_file, 'w') as ofile3:
+            with open(input_file, 'r') as ifile:
+                for line in ifile:
+                    elements = line.rstrip('\n').split('\t')
+                    evalue = float(elements[9])
+                    isoform = elements[0].split('|')[0]
+                    # split the isoform number off from the end of each ID
+                    gene = isoform.rsplit('_', 1)[0]
+                    # set e value cutoff for blast hits
+                    evalue_cutoff = 0.00001
+                    if evalue <= evalue_cutoff:
+                        ofile3.write(line)
+                        annotation = elements[14].strip('\x00')
+                        elements[14] = annotation
+                        # If the gene already exists in the gene dictionary, use the hit with the lower evalue
+                        if bool(gene in gene_dict.keys()) is True:
+                            # print(gene, gene_dict[gene][9])
+                            # print(float(gene_dict[gene][9]), float(evalue))
+                            # print(float(gene_dict[gene][9]) > float(evalue))
+                            # I am using the absolute best hit for each gene based on its evalue
+                            if float(gene_dict[gene][9]) > float(evalue):
+                                gene_dict[gene] = elements
+                        else:
+                            gene_dict[gene] = elements
+                    else:
+                        ofile2.write(line)
+
+            ifile.close()
+        ofile3.close()
+    ofile2.close()
 
     # Write output file with best hit for each gene
     with open(output_file, 'w') as ofile:
@@ -44,6 +56,10 @@ def convert(input_file):
             new_string += '\n'
             ofile.write(new_string)
     ofile.close()
+
+
+
+
 
 
 if __name__ == '__main__':
